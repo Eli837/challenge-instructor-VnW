29edbe06095f88f194a69d3b60c503df59da032e
elizete <elizeteneuzaa@gmail.com>
2021-01-26 16:47:40 -0300

    finalizando o desafio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import './App.css';
+import React, {Component} from 'react';
+import "./App.css";
+import "./index.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>challenge-instructor-VnW</h1>
-    </div>
-  );
+
+
+
+const apiKey= axios.create ({
+   baseURL:"https://newsapi.org/v2/top-headlines?country=us&apiKey=2e2afcce570a4797a7bfe6c3efd4f783",
+});
+
+ 
+ class keydex extends Component{
+
+ state = {
+ keylist: []
+
+ };
+
+  async componentDidMount() {
+ const response = await apiKey.get();
+
+   console.log(response.data.articles);
+
+   this.setState({
+    keylist: response.data.articles
+
+   });
+
+  }
+  
+  render() {
+    return (
+        
+      
+      <div className= "box-geral">
+        {this.state.keylist.map((item, index) => (
+          <div  key={index}>
+            <p className= "autor">{item.author}</p>
+           <img className="img" src={item.urlToImage} />
+           <p className= "box-titulo">{item.title}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+    
+export default keydex;
